9745adbab8866d4b1f25e81e017fcddb970dfbc1
Raymond <raym0nd@outlook.com>
2018-04-02 15:20:44 -0400

    part of add function, get_tag_dir, create_tag_folder

--- tags.c
+++ tags.c
@@ -23,32 +23,45 @@ int usage()
     return 0;
 }
 
-char *get_tags_dir()
+char *get_tags_dir() // note: remember to free *basedir
 {
 
     struct passwd *pws;
     pws = getpwuid(geteuid());
-    char *dir = (char *)malloc(50 * sizeof(char));
-    // init dir
+    char *dir = (char *)malloc(50 * sizeof(char)); // undone: check if dir is too long, over 50
 
     strcpy(dir, "/home/");
     strcat(dir, pws->pw_name); // get dir with user name
     strcat(dir, "/.tags");
 
-    return dir; //note: remember to free!!
+    return dir;
 }
 
-int get_tag_dir() {}
+char *get_tag_dir(char *tagname) // note: remember to free *tagdir
+{
+    char *tagdir = (char *)malloc(50 * sizeof(char));
+    // undone: check if dir is too long, over 50
+    char *basedir = get_tags_dir();
+
+    strcpy(tagdir, basedir);
+    strcat(tagdir, "/");
+    strcat(tagdir, tagname);
+
+    free(basedir);
+    return tagdir;
+}
 
 void check_creat_base_folder()
 {
-    char *dir = get_tags_dir();
+    char *basedir = get_tags_dir();
 
     struct stat st = {0};
-    if (stat(dir, &st) == -1)
+    if (stat(basedir, &st) == -1)
     {
-        mkdir(dir, 0700);
-        // int check = mkdir(dir, 0700);
+        mkdir(basedir, 0700);
+
+        // int check = mkdir(basedir, 0777);
+        // printf("----check base folder: check: %d\n", check);
         // if (check == 0)
         // {
         //     printf("base folder created \n");
@@ -60,10 +73,48 @@ void check_creat_base_folder()
     }
     // else
     // {
-    //     printf("dir already exist\n");
+    //     printf("basedir already exist\n");
     // }
-    free(dir);
+
+    free(basedir);
 }
+
+void check_creat_tag_folder(char *tagname)
+{
+    // note: tmp tag name for testing
+    // char *newtagname = (char *)malloc(50 * sizeof(char));
+    // strcpy(newtagname, "new222tagname");
+
+    char *tagdir = get_tag_dir(tagname);
+
+    struct stat st = {0};
+    if (stat(tagdir, &st) == -1)
+    {
+        mkdir(tagdir, 0777);
+
+        // int check = mkdir(tagdir, 0777);
+        // if (check == 0)
+        // {
+        //     printf("tag folder created \n");
+        // }
+        // else
+        // {
+        //     printf("STH WRONG with create tag folder\n");
+        // }
+    }
+    // else
+    // {
+    //     printf("tagdir already exist\n");
+    // }
+
+    free(tagdir);
+}
+
+int add_tag(char *tagname)
+{
+    check_creat_tag_folder(tagname);
+}
+
 int get_linked_file_name() {}
 
 int creat_tag() {}
@@ -88,9 +139,6 @@ int main(int argc, char *argv[])
     // check & create if base folder exists
     check_creat_base_folder();
 
-
-
-
     if (argc == 1)
     {
         usage();
@@ -100,6 +148,7 @@ int main(int argc, char *argv[])
     if (strcmp(argv[1], "add") == 0)
     {
         printf("add \n");
+        check_creat_tag_folder(argv[2]);
     }
     else if (strcmp(argv[1], "remove") == 0)
     {
