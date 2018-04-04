1a5f0270993ed74a6595b28722327156f99f9f2a
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2018-04-03 20:30:54 -0400

    version 1

--- tags.c
+++ tags.c
@@ -16,12 +16,24 @@ int usage()
            "tags <option> <arguments> \n"
            "Options: \n"
            "    add <filepath> <tagname>        - Link a tag with a file \n"
-           "    remove <filepath> <tagname>     - Remove a tag from file \n"
+           "    remove | rm <filepath> <tagname>     - Remove a tag from file \n"
            "    addtag | at <tagname>                - Create a new tag \n"
            "    removetag | rmt <tagname>             - Delete a tag \n"
            "    list | ls                            - List all tags or list for a file \n"
            "    search <tagname>                - Search all files with a tag name \n");
     return 0;
+}
+
+char *get_current_directory(){
+    char *buffer;
+    char *dir;
+    long size;
+
+    size = pathconf(".", _PC_PATH_MAX);
+    if ((buffer = (char *)malloc((size_t)size)) != NULL)
+        dir = getcwd(buffer, (size_t)size);
+    //printf("current dir is %s\n", dir);
+    return dir;
 }
 
 char *get_tags_dir() // note: remember to free *basedir
@@ -113,14 +125,14 @@ void check_create_tag_folder(char *tagName)
 char *tag_exists(char *tagName) {
     char *tagPath = get_tag_dir(tagName);
     if (access(tagPath, F_OK) != -1){
-        printf("exist %s\n", tagPath);
+        //printf("exist %s\n", tagPath);
         return tagPath;
     }
     else
     return NULL;
 }
 
-int get_linked_file_name() {}
+int get_linked_file_name(char *tagName) {}
 
 int create_tag(char *tagName) {
     check_create_tag_folder(tagName);
@@ -130,6 +142,7 @@ int delete_tag(char *tagName) {
     char *tag = tag_exists(tagName);
     char cmd[256];
     if ( tag == NULL){
+        printf("tag %s does not exist\n", tagName);
         return -1;
     }
 
@@ -139,11 +152,84 @@ int delete_tag(char *tagName) {
     system(cmd);
 }
 
-int add_file_tag(char *tagName) {
-    //check_create_tag_folder(tagName);
+int add_file_tag(char *filePath, char *tagName) {
+    char *tag = tag_exists(tagName);
+    if (access(filePath, F_OK) == -1){
+        printf("file %s does not exist\n", filePath);
+        return -1;
+    }
+    if ( tag == NULL) {
+        printf("tag %s does not exist, create one\n", tagName);
+        check_create_tag_folder(tagName);
+    }
+    char *fileDir = get_tag_dir(tagName); 
+    strcat(fileDir, "/");
+    strcat(fileDir, filePath);
+
+    struct stat st = {0};
+    if (stat(fileDir, &st) == -1)
+    {
+        char cmd[256];
+        memset(cmd, '\0', sizeof(cmd));
+        strcpy(cmd, "touch ");
+        strcat(cmd, fileDir);
+
+        FILE *fp = fopen(fileDir, "w");
+        if (fp == NULL)
+        {
+            printf("Error opening file!\n");
+            exit(1);
+        }else{
+            fputs(get_current_directory(), fp);
+        }
+        fclose(fp);
+    }
+    else
+    {
+        printf("tag %s has already been added to file %s\n",tagName, filePath);
+    }
 }
 
-int search_tag() {}
+int search_file_for_tag(char *tagName) {
+    char tagFile[256];
+    char *tag = tag_exists(tagName);
+    memset(tagFile, '\0', sizeof(tagFile));
+    if (tag == NULL){
+        printf("tag %s does not exist\n", tagName);
+        return -1;
+    }
+    else 
+    {
+        strcpy(tagFile, tag);
+        struct dirent *de; 
+        FILE *fp;
+        char buff[256];
+        DIR *dr = opendir(tagFile);
+        char fileName[256];
+        memset(fileName, '\0', sizeof(fileName));
+        if (dr == NULL) 
+        {
+            printf("Could not open current directory" );
+            return 0;
+        }
+
+        while ((de = readdir(dr)) != NULL) {
+            if ((strcmp(de->d_name, ".") !=0) && (strcmp(de->d_name, "..") !=0))
+            { 
+                printf("file name: %s; ", de->d_name);    
+                strcpy(fileName, tagFile);
+                strcat(fileName, "/");
+                strcat(fileName, de->d_name);
+                fp = fopen(fileName, "r");
+                fscanf(fp, "%s", buff);
+                printf("location:%s\n",buff );
+            }
+        }
+ 
+    closedir(dr);    
+    }
+
+}
 
 int list_tags() {
     struct dirent *de; 
@@ -174,7 +260,21 @@ int list_tags() {
     return 0;
 }
 
-int remove_file_tag() {}
+int remove_file_tag(char *filePath, char *tagName) {
+    char *tag = tag_exists(tagName);
+    if (access(filePath, F_OK) == -1){
+        printf("file %s does not exist in current directory\n", filePath);
+        return -1;
+    }
+    if ( tag == NULL) {
+        printf("tag %s does not exist\n", tagName);
+        return -1;
+    }
+    strcat(tag, "/");
+    strcat(tag, filePath);
+    remove(tag);
+
+}
 
 int remove_all_tag(){
 
@@ -196,6 +296,7 @@ int main(int argc, char *argv[])
 
     // check & create if base folder exists
     check_create_base_folder();
+    //get_current_directory();
 
     if (argc == 1)
     {
@@ -206,12 +307,22 @@ int main(int argc, char *argv[])
     if (strcmp(argv[1], "add") == 0)
     {
         printf("add \n");
-        add_file_tag(argv[2]);
+        if (argc != 4){
+            printf("wrong input\n");
+            usage();
+            return -1;
+        }
+        add_file_tag(argv[2], argv[3]);
     }
-    else if (strcmp(argv[1], "remove") == 0)
+    else if ((strcmp(argv[1], "remove") == 0) || (strcmp(argv[1], "rm") == 0))
     {
         printf("remove \n");
-        remove_file_tag();
+        if (argc != 4){
+            printf("wrong input\n");
+            usage();
+            return -1;
+        }
+        remove_file_tag(argv[2], argv[3]);
     }
     else if ((strcmp(argv[1], "addtag") == 0) || (strcmp(argv[1], "at") == 0))
     {
@@ -223,9 +334,10 @@ int main(int argc, char *argv[])
         printf("removetag \n");
         delete_tag(argv[2]);
     }
-    else if (strcmp(argv[1], "search") == 0)
+    else if ((strcmp(argv[1], "search") == 0) || (strcmp(argv[1], "se") == 0))
     {
         printf("search \n");
+        search_file_for_tag(argv[2]);
     }
     else if ((strcmp(argv[1], "list") == 0) || (strcmp(argv[1], "ls") == 0))
     {
@@ -247,12 +359,7 @@ int main(int argc, char *argv[])
         usage();
     }
 
-    char cwd[1024];
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-        fprintf(stdout, "Current working dir: %s\n", cwd);
-    else
-        perror("getcwd() error");
-
+    
     return 0;
 }
 
